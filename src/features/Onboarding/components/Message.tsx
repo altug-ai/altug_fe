be367d52f8d9af6b898199692b9a990aa9dcbe18
altug-ai <newaltug@gmail.com>
2024-08-30 18:03:45 +0100

    checking audio

--- src/features/Onboarding/components/Message.tsx
+++ src/features/Onboarding/components/Message.tsx
@@ -217,7 +217,7 @@ const Message = ({ system, user, message, date, image, premium, voice, audioRef,
                                 <Button
                                     className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
                                     onClick={() => {
-                                        const utterance = new SpeechSynthesisUtterance("Audio Enabled");
+                                        const utterance = new SpeechSynthesisUtterance("");
                                         window.speechSynthesis.cancel();
                                         window.speechSynthesis.speak(utterance);
                                         if (audioRef?.current) {
