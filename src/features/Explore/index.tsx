9e25caae3e6ab2176975aacd3fcaa31ac560fae2
altug-ai <newaltug@gmail.com>
2024-09-02 17:20:12 +0100

    done add audio

--- src/features/Explore/index.tsx
+++ src/features/Explore/index.tsx
@@ -7,14 +7,15 @@ import { useContext, useState } from 'react';
 import { TbLoader3 } from 'react-icons/tb';
 import PlayerCard from '../Profile/components/PlayerCard';
 import TabBar from '../Profile/components/TabBar';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 type Props = {}
 
 const Explore = (props: Props) => {
     const [route, setRoute] = useState<number>(0)
     const { loading } = useContext(AuthContext)
-    const { data: Coaches, loading: CoachLoad } = useGetCoaches()
-    const { data: Players, loading: PlayerLoad } = useGetPlayers()
+    const { data: Coaches, loading: CoachLoad, hasMore, loadMore } = useGetCoaches()
+    const { data: Players, loading: PlayerLoad, hasMore: phasMore, loadMore: ploadMore } = useGetPlayers()
     const t = useTranslations('Home.Explore');
 
 
@@ -32,7 +33,7 @@ const Explore = (props: Props) => {
                     <div onClick={() => { setRoute(1) }} className={`w-1/2 h-full  ${route === 1 && "bg-[#357EF8] rounded-[38px]"} cursor-pointer  grid place-items-center`}>
                         <h1 className='text-[16px] font-semibold leading-[12.13px] tracking-[0.3%] text-[#FFFFFF]'>{t("Professional")}</h1>
                     </div>
-                </div>  
+                </div>
             </div>
 
 
@@ -59,11 +60,28 @@ const Explore = (props: Props) => {
             {
                 route === 0 && (
                     <div className='w-full flex flex-col max-w-[388px] mt-[30px] mb-[50px]'>
-                        {
-                            Coaches?.map((coach: any) => {
-                                return (<PlayerCard point={coach?.attributes?.point} id={coach?.id} name={coach?.attributes?.name} coach picture={coach?.attributes?.profile?.data?.attributes?.url ?? coach?.attributes?.pic_url} clubLogo={coach?.attributes?.club?.data?.attributes?.logo?.data?.attributes?.url} key={coach.id} />)
-                            })
-                        }
+                        <InfiniteScroll
+                            dataLength={Coaches.length}
+                            next={loadMore}
+                            hasMore={hasMore}
+                            loader={
+                                <div className='max-w-[388px] w-full my-[30px] flex justify-center'>
+                                    <TbLoader3 className="text-white w-10 h-10 animate-spin" />
+                                </div>
+                            }
+
+                            endMessage={<p className='text-center my-2 text-slate-400'></p>}
+                        >
+                            {
+                                Coaches?.map((coach: any) => {
+
+                                    return (
+                                        <PlayerCard key={coach.id} point={coach?.attributes?.point} id={coach?.id} name={coach?.attributes?.name} coach picture={coach?.attributes?.profile?.data?.attributes?.url ?? coach?.attributes?.pic_url} clubLogo={coach?.attributes?.club?.data?.attributes?.logo?.data?.attributes?.url} />
+
+                                    )
+                                })
+                            }
+                        </InfiniteScroll>
                     </div>
                 )
             }
@@ -71,11 +89,27 @@ const Explore = (props: Props) => {
             {
                 route === 1 && (
                     <div className='w-full flex flex-col max-w-[388px] mt-[30px] mb-[50px]'>
-                        {
-                            Players?.map((player: any) => {
-                                return (<PlayerCard point={player?.attributes?.point} id={player?.id} position={player?.attributes?.position} name={player?.attributes?.name} picture={player?.attributes?.profile?.data?.attributes?.url ?? player?.attributes?.pic_url} clubLogo={player?.attributes?.club?.data?.attributes?.logo?.data?.attributes?.url} key={player.id} />)
-                            })
-                        }
+                        <InfiniteScroll
+                            dataLength={Coaches.length}
+                            next={ploadMore}
+                            hasMore={phasMore}
+                            loader={
+                                <div className='max-w-[388px] w-full my-[30px] flex justify-center'>
+                                    <TbLoader3 className="text-white w-10 h-10 animate-spin" />
+                                </div>
+                            }
+                            endMessage={<p className='text-center my-2 text-slate-400'></p>}
+                        >
+                            {
+                                Players?.map((player: any) => {
+                                    return (
+
+                                        <PlayerCard key={player.id} point={player?.attributes?.point} id={player?.id} position={player?.attributes?.position} name={player?.attributes?.name} picture={player?.attributes?.profile?.data?.attributes?.url ?? player?.attributes?.pic_url} clubLogo={player?.attributes?.club?.data?.attributes?.logo?.data?.attributes?.url} />
+
+                                    )
+                                })
+                            }
+                        </InfiniteScroll>
                     </div>
                 )
             }
@@ -83,7 +117,7 @@ const Explore = (props: Props) => {
 
             {/* the tab button */}
 
-            <TabBar page='explore' />
+            < TabBar page='explore' />
         </div>
     )
 }
