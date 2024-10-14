64a20588b3b22143d12dbaf58bdb1108d6ac9159
altug-ai <newaltug@gmail.com>
2024-10-14 15:23:04 +0100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { authOptions } from '@/lib/auth';
 import NextAuth from 'next-auth';
-
+export const maxDuration = 60;
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
